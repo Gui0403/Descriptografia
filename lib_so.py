8bb53019ef4c07749c05472c2fe0d8236f02a6a3
Guilherme <167132275+Gui0403@users.noreply.github.com>
2024-06-12 21:48:09 -0400

    Add files via upload

---
+++ lib_so.py
@@ -0,0 +1,19 @@
+a ='banco_01.txt'
+b =127
+c =input('Digite: ')
+def decodificar(nome, frase, valor_cripto):
+    for salto in range(0, 127):
+        new_frase = ''
+        discrito = open('banco_01.txt', 'r')
+        for l in discrito.read():
+            if ord(l)+salto > 127:
+                new_frase += chr((ord(l)+ salto)% 127)
+            else:
+                new_frase += chr((ord(l)+ salto))
+                
+        print(f'Tentativa [{salto}] : {new_frase}\n')
+        save = open('result.txt', 'a')
+        save.write(new_frase)
+        save.close()
+        discrito.close()
+decodificar(a,b,c)
